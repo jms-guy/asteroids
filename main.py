47cf9eb38eba711ccddbf9765e65d2c1eb67fd79
jms-guy <guyjamie92@gmail.com>
2024-09-06 13:11:03 -0400

    Added base class, player subclass

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x1 = SCREEN_WIDTH / 2
+    y1 = SCREEN_HEIGHT / 2
+    player = Player(x1, y1, PLAYER_RADIUS)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,7 +22,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        clock.tick(60)
+        dt = (clock.tick(60)) / 1000
 
 
 if __name__ == "__main__":
