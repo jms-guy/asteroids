71f94cfc2a2d45731ec591203686f57fe541d464
jms-guy <guyjamie92@gmail.com>
2024-09-11 13:55:41 -0400

    Added asteroids, working on shot

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+#Class for asteroids
+
+from circleshape import *
+from constants import *
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -2,15 +2,28 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import * 
 
 def main():
+    #Game screen initialization
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
+    #Container groups for classes
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
 
     x1 = SCREEN_WIDTH / 2
@@ -21,12 +34,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         for obj in updatable:
             obj.update(dt)
+
+        for obj in asteroids:
+            if obj.is_colliding(player):
+                print("Game over!")
+                return
 
         pygame.Surface.fill(screen, (0,0,0))
 
